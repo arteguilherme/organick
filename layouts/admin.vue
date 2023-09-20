67864a352b9990d4f2a4d4f3865a8b3c15d6a330
DESKTOP-TTDPIMI\Guilherme <totem.guilhermeca@sfiemt.ind.br>
2023-09-20 13:37:58 -0400

    Format

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,8 +1,7 @@
-
 <template>
-    <AdminHeader />
-    <AdminSidebar />
-    <div class="ml-64 mx-auto p-4">
-        <slot />
-    </div>
+  <AdminHeader />
+  <AdminSidebar />
+  <div class="ml-64 mx-auto p-4">
+    <slot />
+  </div>
 </template>
--- pages/admin/products/create.vue
+++ pages/admin/products/create.vue
@@ -1,12 +1,15 @@
 <script setup>
 definePageMeta({
-    layout: 'admin'
-})
+  layout: "admin",
+});
 </script>
 <template>
-    <NuxtLayout>
-        <!-- Aquí puedes agregar el contenido principal de tu página CRM -->
-        <h1 class="text-2xl font-bold mb-4">¡Bienvenido al CRM de Mi Empresa!</h1>
-        <p>En esta sección encontrarás todo lo que necesitas para administrar tus clientes y ventas de manera eficiente.</p>
-    </NuxtLayout>
+  <NuxtLayout>
+    <!-- Aquí puedes agregar el contenido principal de tu página CRM -->
+    <h1 class="text-2xl font-bold mb-4">¡Bienvenido al CRM de Mi Empresa!</h1>
+    <p>
+      En esta sección encontrarás todo lo que necesitas para administrar tus
+      clientes y ventas de manera eficiente.
+    </p>
+  </NuxtLayout>
 </template>
